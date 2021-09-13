9ab7177daa2f3f255f2e15613c650cf9d49931b8
Siniša Nimčević <sinimch@gmail.com>
2021-09-13 23:50:52 +0200

    chore: refactor

--- src/components/ListItem/ListItem.test.js
+++ src/components/ListItem/ListItem.test.js
@@ -1,16 +1,10 @@
-import { render } from '@testing-library/react';
-import { Provider } from 'react-redux';
-import store from '../../redux/stores/store';
+import { renderWrapped } from '../../services/testUtils';
 import ListItem from './ListItem';
 
 const dummyItem = {
   id: 1,
   title: 'Top Kek',
   isActive: false,
-};
-
-const renderWrapped = (children) => {
-  return render(<Provider store={store}>{children}</Provider>);
 };
 
 describe('GIVEN ListItem', () => {
